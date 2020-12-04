c862c0156466dcd242aabea9d2243330ced3ec81
Pierre Bréchet <brechet@mis.mpg.de>
2020-12-04 16:28:35 +0100

    update version number in notebook

--- jln2jupyter.jl
+++ jln2jupyter.jl
@@ -17,7 +17,7 @@ end
 function ipynb_structure()
   ipynb = Dict{String,Any}()
   kernelspec = Dict{String,String}("language" => "julia",
-    "name" => "julia-0.6", "display_name" => "Julia 0.6")
+                                   "name" => "julia-$VERSION", "display_name" => "Julia $(VERSION.major).$(VERSION.minor)")
   ipynb["metadata"] = Dict{String,Any}("kernelspec" => kernelspec)
   ipynb["nbformat"] = 4
   ipynb["nbformat_minor"] = 0
@@ -32,11 +32,12 @@ function test_output(output)
 end
 
 function jl2cells(filename)
-  if v"0.6" <= VERSION < v"0.7"
-      # chomp true by default?
-    lines = collect(readlines(filename, chomp=false))
-else
+  if VERSION < v"0.6"
     lines = collect(readlines(filename))
+elseif VERSION < v"0.7"
+    lines = collect(readlines(filename, chomp=false))
+else  # keep keyword
+    lines = collect(readlines(filename, keep=true))
   end
   cells = Vector{Vector{String}}()
   block = Vector{String}()
